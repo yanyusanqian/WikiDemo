eb77999269e377da60a7c69a4a77b2ef4137a2db
yanyusanqian <1050011046@qq.com>
2023-10-11 17:47:37 +0800

    新增后端分页

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.utils.CopyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -16,6 +20,7 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -26,7 +31,15 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(ebookReq.getName())) {
             criteria.andNameLike("%" + ebookReq.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        // 总行数
+        LOG.info("总行数:{}",pageInfo.getTotal());
+        // 总页数
+        LOG.info("总行数:{}",pageInfo.getPages());
+
         // List<EbookResp> respList = new ArrayList<>();
         // for (Ebook ebook : ebookList) {
         //     // EbookResp ebookResp = new EbookResp();
