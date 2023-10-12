7d46b7c302aa1268320fc61f5e4defe98aa028c3
yanyusanqian <1050011046@qq.com>
2023-10-12 17:08:28 +0800

    电子书管理页面前后端分页整合成功

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -115,13 +115,19 @@ export default defineComponent({
             loading.value = true;
             // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
             ebooks.value = [];
-            axios.get("/ebook/list", params).then((response) => {
+            axios.get("/ebook/list", {
+                params:{
+                    page:params.page,
+                    size:params.size
+                }
+            }).then((response) => {
                 loading.value = false;
                 const data = response.data;
-                ebooks.value = data.content;
+                ebooks.value = data.content.list;
 
                 // 重置分页按钮
                 pagination.value.current = params.page;
+                pagination.value.total = data.content.total;
             });
         };
 
@@ -137,7 +143,11 @@ export default defineComponent({
         };
 
         onMounted(() => {
-            handleQuery({});
+            handleQuery({
+                // 和后端名称要一致才能正确映射
+                page: 1,
+                size: pagination.value.pageSize
+            });
         })
 
         return {
