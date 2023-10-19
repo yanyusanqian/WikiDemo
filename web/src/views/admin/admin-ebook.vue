18da6d7de7d046af8a62472f9aaa8cc91b242b73
yanyusanqian <1050011046@qq.com>
2023-10-19 15:28:56 +0800

    增加按名字查询电子书功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,11 +3,26 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            <P>
-                <a-button type="primary" @click="add()" size="large">
-                    新增
-                </a-button>
-            </P>
+            <p>
+            <a-form layout="inline" :model="param">
+                <a-form-item>
+                    <a-input v-model:value="param.name" placeholder="请输入查询名称">
+                    </a-input>
+                </a-form-item>
+
+                <a-form-item>
+                    <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+                        查询
+                    </a-button>
+                </a-form-item>
+                <a-form-item>
+                    <a-button type="primary" @click="add()">
+                        新增
+                    </a-button>
+                </a-form-item>
+            </a-form>
+            </p>
+
             <a-table
                     :columns="columns"
                     :row-key="record => record.id"
@@ -81,6 +96,8 @@ export default defineComponent({
         }
     },
     setup() {
+        const param = ref();
+        param.value = {};
         const ebooks = ref();
         const pagination = ref({
             current: 1,
@@ -137,7 +154,8 @@ export default defineComponent({
             axios.get("/ebook/list", {
                 params: {
                     page: params.page,
-                    size: params.size
+                    size: params.size,
+                    name: param.value.name
                 }
             }).then((response) => {
                 loading.value = false;
@@ -240,6 +258,9 @@ export default defineComponent({
             modalLoading,
             handleModalOk,
             ebook,
+
+            param,
+            handleQuery,
         }
     }
 });
