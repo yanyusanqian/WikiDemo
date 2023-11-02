b46232f46d65db8d1a2e1c197b7ecd16b717e735
san <1050011046@qq.com>
2023-11-02 13:52:47 +0800

    文档管理增加预览编辑功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -81,12 +81,20 @@
                         <a-form-item label="顺序">
                             <a-input v-model:value="doc.sort" placeholder="请输入顺序"/>
                         </a-form-item>
+                        <a-form-item>
+                            <a-button type="primary" @click="handlePreviewContent()">
+                                <EyeOutlined/> 内容预览
+                            </a-button>
+                        </a-form-item>
                         <a-form-item label="内容">
                             <div id="content"></div>
                         </a-form-item>
                     </a-form>
                 </a-col>
             </a-row>
+            <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
+                <div class="wangeditor" :innerHTML="previewHtml"></div>
+            </a-drawer>
         </a-layout-content>
     </a-layout>
 
@@ -364,6 +372,19 @@ export default defineComponent({
             });
         };
 
+        // ----------------富文本预览--------------
+
+        const drawerVisible = ref(false);
+        const previewHtml = ref();
+        const handlePreviewContent = () => {
+            const html = editor.txt.html();
+            previewHtml.value = html;
+            drawerVisible.value = true;
+        };
+        const onDrawerClose = () => {
+            drawerVisible.value = false;
+        };
+
 
         onMounted(() => {
             handleQuery();
@@ -389,6 +410,11 @@ export default defineComponent({
             handleQuery,
 
             treeSelectData,
+
+            drawerVisible,
+            previewHtml,
+            handlePreviewContent,
+            onDrawerClose,
         }
     }
 });
