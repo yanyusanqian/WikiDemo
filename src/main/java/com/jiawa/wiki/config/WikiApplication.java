4c0fb2d12f4f67e86bdf41fcc8a3072604977ed5
san <1050011046@qq.com>
2023-11-28 09:38:12 +0800

    开启定时任务，编写定时器测试代码

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan("com.jiawa")
 @SpringBootApplication
 @MapperScan(basePackages = "com.jiawa.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
 
     public static void main(String[] args) {
